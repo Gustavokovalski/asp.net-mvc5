4f92993cb58160c6fa1c9f80a52464afa9d3f816
Gustavokovalski <gustavokovalski.saporiti@gmail.com>
2019-06-12 10:32:14 -0300

    Classes, DAO e migrations

---
+++ ControleFinancas/ControleFinancas/DAO/ControleFinancasContext.cs
@@ -0,0 +1,22 @@
+﻿using ControleFinancas.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Web;
+using System.Web.Configuration;
+
+namespace ControleFinancas.DAO
+{
+    public class ControleFinancasContext : DbContext
+    {
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet <Movimentacao> Movimentacoes { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+           modelBuilder.Entity<Movimentacao>().HasRequired(m => m.Usuario);
+        }
+    }
+}
---
+++ ControleFinancas/ControleFinancas/Entidades/Tipo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ControleFinancas.Entidades
+{
+    public enum Tipo
+    {
+        Entrada, Saida
+    }
+}
