011476a1338df9a7a491e6bfc920e56275c84b68
Gustavokovalski <gustavokovalski.saporiti@gmail.com>
2019-06-12 11:25:29 -0300

    Layout e cadastro de usuários

--- ControleFinancas/ControleFinancas/Controllers/HomeController.cs
+++ ControleFinancas/ControleFinancas/Controllers/HomeController.cs
@@ -16,3 +16,4 @@ namespace ControleFinancas.Controllers
             return View();
         }
     }
+}
---
+++ ControleFinancas/ControleFinancas/Controllers/UsuarioController.cs
@@ -0,0 +1,44 @@
+﻿using ControleFinancas.DAO;
+using ControleFinancas.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ControleFinancas.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        private UsuarioDAO usuarioDAO;
+
+        public UsuarioController(UsuarioDAO usuarioDAO)
+        {
+            this.usuarioDAO = usuarioDAO;
+        }
+
+        // GET: Usuario
+        public ActionResult Form()
+        {
+            return View();
+        }
+
+        public ActionResult Adiciona(Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                usuarioDAO.Adiciona(usuario);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View("Form", usuario);
+            }
+        }
+        public ActionResult Index()
+        {
+            IList<Usuario> usuarios = usuarioDAO.Lista();
+            return View(usuarios);
+        }
+    }
+}
